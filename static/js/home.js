1f3e6b49165db44e5d37a21fb3d5804bc89a84db
leejungho9 <9wjdgh9@naver.com>
2022-07-02 03:13:47 +0900

    create project

---
+++ static/js/home.js
@@ -0,0 +1,67 @@
+/*header js*/
+
+console.log("시작");
+let sidebar = false;
+let sidebarEl = document.querySelector(".nav-menu");
+
+let workMenu = false;
+let workMenuEl = document.querySelector(".work-menu");
+let workMenuUl = document.querySelector(".work-menu-ul");
+
+function setworkMenu() {
+  workMenu = true;
+}
+
+function showSidebar() {
+  console.log("햄버거 버튼 클릭" + sidebar);
+  sidebar = !sidebar;
+  sidebar
+    ? (sidebarEl.className = "nav-menu active")
+    : (sidebarEl.className = "nav-menu");
+}
+
+function showworkMenu() {
+  console.log("work 버튼 클릭" + workMenu);
+  workMenu = !workMenu;
+  workMenu
+    ? (workMenuEl.className = "work-menu active")
+    : (workMenuEl.className = "work-menu");
+
+  workMenu
+    ? (workMenuUl.className = "work-menu-ul active")
+    : (workMenuUl.className = "work-menu-ul");
+}
+
+let path = window.location.pathname;
+console.log(path);
+if (sidebar === true) {
+  setworkMenu(false);
+  setworkMenu();
+}
+
+let artgoodsEl = document.querySelector(".a_artgoods");
+let calendarEl = document.querySelector(".a_calendar");
+let diaryEl = document.querySelector(".a_diary");
+let packageEl = document.querySelector(".a_package");
+let cardEl = document.querySelector(".a_card");
+let catalogueEl = document.querySelector(".a_catalogue");
+
+if (path == "/html/artgoods.html") {
+  showworkMenu();
+  artgoodsEl.className = "detailMenu active";
+} else if (path == "/html/calendar.html") {
+  showworkMenu();
+  calendarEl.className = "detailMenu active";
+} else if (path == "/html/diary.html") {
+  showworkMenu();
+  diaryEl.className = "detailMenu active";
+} else if (path == "/html/package.html") {
+  showworkMenu();
+  packageEl.className = "detailMenu active";
+} else if (path == "/html/card.html") {
+  showworkMenu();
+  cardEl.className = "detailMenu active";
+} else if (path == "/catalogue") {
+  showworkMenu();
+  catalogueEl.className = "detailMenu active";
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const copyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./home.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "static",
+        },
+      ],
+    }),
+  ],
+
+  devServer: {
+    host: "localhost",
+  },
+};
